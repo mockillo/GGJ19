bcb2ec1fcfa6402076345c6e0938f0c1c596d5da
Alexander Polden <mockillo@Alexanders-MacBook-Pro.local>
2019-01-26 20:47:53 +0100

    Fixed camera max width on Joe. Added support for controller and keyboard on menu. Got some sounds from Ludvig.

--- src/Scenes/MainMenu.gd
+++ src/Scenes/MainMenu.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 var paused = false
 
+onready var btns = [
+	$VBoxContainer/Quit,
+	$VBoxContainer/Play
+	]
+
+var current_btn = 1
+
 func set_paused(p: bool):
 	paused = p
 	
@@ -14,3 +21,16 @@ func play():
 	
 func quit():
 	get_tree().quit()
+	
+func _process(delta):
+	if (Input.is_action_pressed("ui_up") && current_btn < btns.size() - 1):
+		current_btn += 1
+	elif (Input.is_action_pressed("ui_down") && current_btn > 0):
+		current_btn -= 1
+		
+	print(btns)
+	print(current_btn)
+	print(btns[current_btn])	
+	
+	btns[current_btn].grab_focus()
+	
